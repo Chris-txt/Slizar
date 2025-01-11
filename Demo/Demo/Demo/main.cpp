e217395aa6bfc716c9cebdab9bec2528a954a8cd
Chris-txt <christianmarelli080@gmail.com>
2025-01-11 15:55:50 +0100

    maggiore difficolta, buff e nerf agli oggetti, aggiunti suoni, nuove immagini

--- Demo/Demo/Demo/main.cpp
+++ Demo/Demo/Demo/main.cpp
@@ -1,12 +1,15 @@
 #define IMM2D_WIDTH 150
 #define IMM2D_HEIGHT 100
-#define IMM2D_SCALE 6
+#define IMM2D_SCALE 7
 
 #define IMM2D_IMPLEMENTATION
 #include "immediate2d.h"
+#include <string>
+using namespace std;
 
 #define NUM_ATTACCHI 12
 #define NUM_OGGETTI 3
+#define NUM_RAGGI 2
 
 static const Color DarkRed = MakeColor(88, 0, 0);
 void DrawTitle(int b);
@@ -15,10 +18,11 @@ void DrawScene(int enemyY, int enemyEalth, bool PlayerTurn, int playerEalth, int
 void enemyAnimation(int& enemyY, bool& direction);
 int qualeAzione(int mX, int mY);
 void DrawAttack(int enemyY);
+int setRightAmountOfMoney(int SoldiOggetti);
 void DrawAzione(int NumOggetti[], int b, int& playerEalth, int& enemyEalth, int& SoldiOggetti, bool& PlayerTurn);
 bool DrawSpecial(int b, int enemyEalth);
 void Movement(int& PlayerX, int& PlayerY);
-bool haPresoDanno(int AttackX[], int AttackY, int PlayerX, int PlayerY, int i, int raggioX, bool SuperForma);
+bool haPresoDanno(int AttackX[], int AttackY, int PlayerX, int PlayerY, int i, int raggioX[], bool SuperForma);
 void DrawSuperFormCutscene();
 void DrawDeath(bool isEnemyDead, int b);
 
@@ -27,6 +31,10 @@ static constexpr const char iconanemico[] = "miniNemico.png";
 static constexpr const char mortenemico[] = "nemicoMorto.png";
 static constexpr const char supernemico[] = "nemicoFinale.png";
 static constexpr const char pericolo[] = "pericolo.png";
+static constexpr const char barraspeciale[] = "barraSpeciale.png";
+static constexpr const char raggionemico[] = "raggio.png";
+static constexpr const char attacconemico[] = "attaccoNemico.png";
+static constexpr const char attaccosuper[] = "attaccoSuper.png";
 
 void run() {
 	UseDoubleBuffering(true);
@@ -35,25 +43,36 @@ void run() {
 	bool movingUP = true;									//movingUP serve per decidere se il nemico si muove su o giù
 	bool PlayerTurn = true;									//vede se è il turno del giocatore
 	int enemyEalth = 100, playerEalth = 100;				//vita del nemico e del giocatore
-	bool statoDelMouse = false, PlayerColpito = false;		//prevede che alcuni comandi collegati al mouse e al player vengano ripetute troppo velocemente
+	bool statoDelMouse = false;								//prevede che comandi collegati al mouse vengano ripetuti troppo velocemente
 	int tempo;												//tempo del turno nemico
 	int NumPerOggetto[NUM_OGGETTI] = { 2,7,1 };				//La quantità disponibile per ogni oggetto
 	int SoldiOggetti = 0;
 	bool superFormaAttiva = false;							//indicatore per la forma finale del nemico attova, serve per non ripetere la cutscene
 	Image nemico = LoadImage(nemicopng);					//carica immagine del nemico
+	const Image raggio = LoadImage(raggionemico);
 	const Image warning = LoadImage(pericolo);
+	Image enemyAttack = LoadImage(attacconemico);
 
 	DrawTitle(b);
 
 	//battaglia
 	while (enemyEalth > 0 && playerEalth > 0) {
-		tempo = 350;											//La durata del turno nemico, non so in che misura però
-		int PlayerX = 75, PlayerY = 75;							//posizione del giocatore
-		int PreviousX = PlayerX, PreviousY = PlayerY;			//servono per evitare che il giocatore esca dal campo
-		int AttackY = 30, AttackX[NUM_ATTACCHI];				//La posizione degli attacchi nemici
-		int WarningX = RandomInt(25, 40), raggioX = WarningX;	//attacco speciale della nemico nella forma finale
+		tempo = 350;												//La durata del turno nemico, non so in che misura però
+		int PlayerX = 75, PlayerY = 75;								//posizione del giocatore
+		int PreviousX = PlayerX, PreviousY = PlayerY;				//servono per evitare che il giocatore esca dal campo
+		int AttackY = 30, AttackX[NUM_ATTACCHI];					//La posizione degli attacchi nemici
+		int WarningX[NUM_RAGGI], raggioX[NUM_RAGGI];				//attacco speciale della nemico nella forma finale
+		bool PlayerColpito = false;									//fa in modo che il player abbia un pò di tempo per riprendersi dopo aver preso danno
+		int iframe = 0;												//periodo di immortalità dopo aver preso danno
+		int Danni;
+		string DanniStr;
+
 		for (int i = 0; i < NUM_ATTACCHI; i++) {
 			AttackX[i] = RandomInt(1, IMM2D_WIDTH);
+		}
+		for (int i = 0;i < NUM_RAGGI;i++) {
+			WarningX[i] = RandomInt(10, 40);
+			raggioX[i] = WarningX[i];
 		}
 
 		while (!PlayerTurn) {
@@ -81,42 +100,52 @@ void run() {
 
 			//attacchi:
 			for (int i = 0; i < NUM_ATTACCHI; i++) {
-				DrawRectangle(AttackX[i], AttackY, 5, 5, White, White);
-				if (enemyEalth <= 50) {
-					DrawImage(WarningX, 70, warning);
-					DrawLine(raggioX, 0, raggioX, 100, 15, Yellow);
-					DrawLine(raggioX, 0, raggioX, 100, 5, White);
-				}
+				//DrawRectangle(AttackX[i], AttackY, 5, 5, White, White);
+				DrawImage(AttackX[i], AttackY, enemyAttack);
 				if (PlayerColpito == false) {
 					if (haPresoDanno(AttackX, AttackY, PlayerX, PlayerY, i, raggioX, superFormaAttiva)) {
 						playerEalth -= 10;
 						PlayerColpito = true;
+						PlayMusic(32, 200);
+						iframe = 30;
 					}
 				}
 			}
+			if (enemyEalth <= 50) {
+				for (int i = 0;i < NUM_RAGGI;i++) {
+					DrawImage(raggioX[i] - 7, 0, raggio);
+					DrawImage(WarningX [i] - 3, 70, warning);
+				}
+			}
 			if (AttackY > 85) {
-				for (int i = 0; i < NUM_ATTACCHI; i++) {
+				for (int i = 0; i < NUM_ATTACCHI; i++) {		//genera posizioni di nuovi attacchi
 					AttackX[i] = RandomInt(1, IMM2D_WIDTH);
 				}
 				AttackY = 20;
-				raggioX = WarningX;
-				WarningX = RandomInt(25, 125);
+				for (int i = 0;i < NUM_RAGGI;i++) {				//genera posizioni di nuovi raggi
+					raggioX[i] = WarningX[i];
+					WarningX[i] = RandomInt(25, 125);
+				}
+				PlayMusic(53, 100);
 			}
 			
 			Present();
 			AttackY++;
 
 			tempo--;
+			iframe--;
 			if (tempo == 0 || playerEalth <= 0) {
 				PlayerTurn = true;
 			}
-			if (tempo % 50 == 0) {
+			if (iframe == 0) {
 				PlayerColpito = false;
 			}
 			//Tocchi Finali:
-			Wait(25);
+			Wait(20);
 			Clear();
 		}
+
+		SoldiOggetti = setRightAmountOfMoney(SoldiOggetti);
 
 		while (PlayerTurn && playerEalth > 0) {
 			//Disegni:
@@ -134,11 +163,20 @@ void run() {
 				if (statoDelMouse == false) {
 					//Attacco:
 					if (qualeAzione(mX, mY) == 1) {
+						if (superFormaAttiva) {
+							Danni = RandomInt(5, 10);
+						}
+						else {
+							Danni = RandomInt(10, 15);
+						}
+						string DanniStr = to_string(Danni);
 						DrawAttack(enemyY);
-						DrawString(75, 0, "15", "Comic Sans MC", 15, Black, true);
+						DrawString(75, 0, DanniStr.c_str(), "Comic Sans MC", 15, Black, true);
+						PlayMusic(63, 200);
+						PlayMusic(63, 100);
 						Present();
 						Wait(500);
-						enemyEalth -= 15;
+						enemyEalth -= Danni;
 						PlayerTurn = false;
 						SoldiOggetti += 10;
 					}
@@ -152,14 +190,23 @@ void run() {
 						DrawScene(enemyY, enemyEalth, PlayerTurn, playerEalth, SoldiOggetti, nemico);
 						DrawAttack(enemyY);
 						if (FattoCentro) {
-							enemyEalth -= RandomInt(15, 30);
+							Danni = RandomInt(15, 25);
+							DanniStr = to_string(Danni);
+							enemyEalth -= Danni;
 							SoldiOggetti += 30;
-							DrawString(75, 0, "Bravo", "Comic Sans MC", 13, Black, true);
+							DrawString(75, 0, DanniStr.c_str(), "Comic Sans MC", 13, Black, true);
+							PlayMusic(56, 150);
+							PlayMusic(65, 150);
+							PlayMusic(70, 400);
 						}
 						else {
-							enemyEalth -= RandomInt(0, 3);
+							Danni = RandomInt(0, 3);
+							DanniStr = to_string(Danni) + ":(";
+							enemyEalth -= Danni;
 							SoldiOggetti += RandomInt(0, 5);
-							DrawString(75, 0, ":(", "Comic Sans MC", 15, Black, true);
+							DrawString(75, 0, DanniStr.c_str(), "Comic Sans MC", 15, Black, true);
+							PlayMusic(37, 300);
+							PlayMusic(32, 150);
 						}
 						Present();
 						PlayerTurn = false;
@@ -177,9 +224,11 @@ void run() {
 			Clear();
 		}
 
+		SoldiOggetti = setRightAmountOfMoney(SoldiOggetti);
 		if (superFormaAttiva == false && enemyEalth <= 50) {
 			superFormaAttiva = true;
 			nemico = LoadImage(supernemico);
+			enemyAttack = LoadImage(attaccosuper);
 			DrawSuperFormCutscene();
 		}
 	}
@@ -208,6 +257,8 @@ void DrawTitle(int b)
 		Present();
 		Wait(150);
 	}
+	PlayMusic(58, 700);
+	PlayMusic(52, 1100);
 	for (int i = 0;i < 100;i += 10) {
 		DrawCircle(75, 65, i, Black, 0U);
 		Present();
@@ -293,48 +344,58 @@ void DrawAttack(int enemyY)
 	}
 }
 
+int setRightAmountOfMoney(int SoldiOggetti)
+{
+	if (SoldiOggetti > 100) {
+		return 100;
+	}
+	return SoldiOggetti;
+}
+
 void DrawAzione(int NumOggetti[], int b, int& playerEalth, int& enemyEalth, int& SoldiOggetti, bool& PlayerTurn)
 {
 	//box
+	string oggetti[3] = { "(1) Bomba","(2) Cura","(3) Super Cura" };
+	string costo[3] = { "10","25","40" };
 	DrawBackground(b);
 	DrawRectangle(10, 5, 130, 80, Black, White);						//box
 	DrawString(10, 72, "Premere ESC per uscire", "", 7, White);			//istruzione per uscire
-	DrawString(10, 60, "tenere premuto per selezionare", "", 7, White);	//istruzioni
 	DrawRectangle(25, 92, 100, 6, Red);									//barra della vita persa del giocatore
 	DrawRectangle(25, 92, playerEalth, 6, Green);						//barra della vita del giocatore
 	DrawRectangle(25, 88, 100, 3, DarkGray);							//barra dei soldi ottenibili
 	DrawRectangle(25, 88, SoldiOggetti, 3, LightCyan);					//barra dei soldi ottenuti
-	//oggetto 1
-	DrawString(10, 5, "(1) Bomba", "Comic Sans MC", 10, LightRed);
-	DrawString(120, 5, "10", "", 10, LightRed);
-	//oggetto 2
-	DrawString(10, 22, "(2) Cura", "Comic Sans MC", 10, LightGreen);
-	DrawString(120, 22, "25", "", 10, LightGreen);
-	//oggetto 3
-	DrawString(10, 38, "(3) Super Cura", "Comic Sans MC", 10, LightGreen);
-	DrawString(120, 38, "40", "", 10, LightGreen);
 	
+	int j = 5;
+	for (int i = 0;i < 3;i++) {
+		DrawString(10, j, oggetti[i].c_str(), "Comic Sans MC", 10, LightGreen);
+		DrawString(120, j, costo[i].c_str(), "Comic Sans MC", 10, LightGreen);
+		j += 20;
+	}
+
 	Present();
-	while (LastKey() != 27 && PlayerTurn) {
-		if (LastKey() == '1' && SoldiOggetti >= 10) {
+	char ultimo = LastKey();
+	while (ultimo != 27 && PlayerTurn) {
+		ultimo = LastKey();
+		if (ultimo == '1' && SoldiOggetti >= 10) {
 			SoldiOggetti -= 10;
 			playerEalth -= 5;
 			enemyEalth -= 20;
 			PlayerTurn = false;
 		}
-		if (LastKey() == '2' && SoldiOggetti >= 15) {
+		if (ultimo == '2' && SoldiOggetti >= 15) {
 			SoldiOggetti -= 15;
-			playerEalth += 10;
+			playerEalth += 15;
 			PlayerTurn = false;
 		}
-		if (LastKey() == '3' && SoldiOggetti >= 40) {
-			SoldiOggetti -= 10;
-			playerEalth += 35;
+		if (ultimo == '3' && SoldiOggetti >= 35) {
+			SoldiOggetti -= 35;
+			playerEalth += 40;
 			PlayerTurn = false;
 		}
 		//DEBUG!! solo io posso sapere di questo comando
-		if (LastKey() == 'z') {
-			enemyEalth /= 2;
+		if (ultimo == 'z') {
+			enemyEalth -= 50;
+			PlayerTurn = false;
 		}
 	}
 }
@@ -343,13 +404,10 @@ bool DrawSpecial(int b, int enemyEalth)
 {
 	int barraX = 10;
 	bool versoDestra = true;
+	const Image barraMinigioco = LoadImage(barraspeciale);
 	while (LastKey() != 32) {
 		DrawBackground(b);
-		DrawRectangle(24, 2, 101, 6, Red, LightGray);
-		DrawRectangle(25, 3, enemyEalth, 5, Green, 0U);
-		DrawRectangle(10, 40, 90, 50, Red, White);
-		DrawRectangle(101, 40, 10, 50, Green, White);
-		DrawRectangle(112, 40, 20, 50, Red, White);
+		DrawImage(10, 40, barraMinigioco);
 		DrawString(5, 15, "Premi spazio per confermare", "Comic Sans MC", 8, Black);
 		if (versoDestra) {
 			barraX += 8;
@@ -392,15 +450,15 @@ void Movement(int& PlayerX, int& PlayerY)
 	}
 }
 
-bool haPresoDanno(int AttackX[], int AttackY, int PlayerX, int PlayerY, int i, int raggioX, bool SuperForma)
+bool haPresoDanno(int AttackX[], int AttackY, int PlayerX, int PlayerY, int i, int raggioX[], bool SuperForma)
 {
 	if (PlayerY<AttackY + 4 && PlayerY>AttackY - 5) {
 		if (PlayerX > AttackX[i] - 4 && PlayerX < AttackX[i] + 8) {
 			return true;
 		}
 	}
-	if (SuperForma) {
-		if (PlayerX > raggioX - 11 && PlayerX < raggioX + 10) {
+	if (SuperForma && i < NUM_ATTACCHI) {
+		if (PlayerX > raggioX[i] - 9 && PlayerX < raggioX[i] + 12) {
 			return true;
 		}
 	}
@@ -411,18 +469,21 @@ void DrawSuperFormCutscene()
 {
 	const Image SiluetteNemico = LoadImage(mortenemico);
 	Clear(DarkRed);
-	DrawImage(43, 17, SiluetteNemico);
+	DrawImage(44, 18, SiluetteNemico);
 	Present();
-	Wait(2000);
-	for (int i = 100;i > 0;i -= 10) {
+	Wait(1000);
+	for (int i = 90;i > 0;i -= 10) {
+		Clear(DarkRed);
 		DrawCircle(75, 50, i, MakeColor(255, 255 - i, 0), 0U);
+		DrawImage(44, 18, SiluetteNemico);
+		Present();
+		PlayMusic(75, 70);
+		Wait(500);
 	}
-	DrawImage(43, 17, SiluetteNemico);
+	DrawLine(76, 40, 76, 57, 2, Red);
+	PlayMusic(1500, 10);
 	Present();
-	Wait(2000);
-	DrawLine(74, 42, 74, 55, 2, Red);
-	Present();
-	Wait(2000);
+	Wait(1500);
 }
 
 void DrawDeath(bool isEnemyDead, int b)
@@ -432,16 +493,24 @@ void DrawDeath(bool isEnemyDead, int b)
 		Clear(Red);
 		DrawImage(43, 17, NemicoUcciso);
 		Present();
+		PlayMusic(45, 2000);
 		Wait(2000);
 		DrawLine(55, 20, 90, 80, 9, Red);
 		Present();
+		PlayMusic(30, 3000);
 		Wait(3000);
 		Clear();
 		DrawBackground(b);
 		DrawString(75, 25, "Vittoria", "Comic Sans MC", 20, Green, true);
+		PlayMusic(56, 300);
+		PlayMusic(67, 300);
+		PlayMusic(67, 200);
+		PlayMusic(80, 100);
 	}
 	else {
 		DrawBackground(b);
 		DrawString(75, 25, "Game Over", "Comic Sans MC", 20, Red, true);
+		PlayMusic(45, 2000);
+		PlayMusic(30, 3000);
 	}
 }
